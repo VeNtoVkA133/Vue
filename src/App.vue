6d9c9267a98a1579daa6050bd23ae802e9033c31
ventParus <svyatoslav.stolyarov.s.sss2000@mail.ru>
2024-12-09 15:53:51 +0300

    сделал верхнюю часть main блока, и стилизовал, без адаптива

--- src/App.vue
+++ src/App.vue
@@ -82,26 +82,77 @@ import { RouterLink, RouterView } from 'vue-router'
 
       <!--Основная часть-->
       <main class="main">
-        <figure class="main-info">
-          <div class="main-info-wrap">
-            <figure class="main-info-information">
-              <div class="main-info-information-wrap--txt">
-                <img src="./assets/images/minilogo.jpg" alt="minilogo" class="main-info-information-minilogo">
-                <span class="main-info-information-surprise">Фильтр масла в подарок</span>
-                <button class="main-info-information-details">
-                  подробнее
-                  <svg width="39" height="22" viewBox="0 0 39 22" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M0 10.0833H35.1739L26.9043 1.45176L28.4783 0L39 11L28.4783 22L27.0435 20.5652L35.1739 11.9167H0V10.0833Z"/>
+        <section class="main-section1">
+          <figure class="main-section1-info">
+            <div class="main-section1-info-wrap">
+              <figure class="main-section1-info-information">
+                <div class="main-section1-info-information-wrap--txt">
+                  <img src="./assets/images/minilogo.jpg" alt="minilogo" class="main-section1-info-information-minilogo" width="122" height="33">
+                  <span class="main-section1-info-information-surprise">Фильтр масла<br>в подарок</span>
+                  <button class="main-section1-info-information-details">
+                    <span>подробнее</span>
+                    <svg width="39" height="22" viewBox="0 0 39 22" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M0 10.0833H35.1739L26.9043 1.45176L28.4783 0L39 11L28.4783 22L27.0435 20.5652L35.1739 11.9167H0V10.0833Z"/>
+                    </svg>
+                  </button>
+                </div>
+
+                <img src="./assets/images/oilbg.png" alt="oilbg" class="main-section1-info-information-oilbg">
+
+                <div class="main-section1-info-information-wrap--txt">
+                  <div class="main-section1-info-information-wrap--txt-btn">
+                    <button class="main-section1-info-information-btn">
+                      <svg width="34" height="24" viewBox="0 0 34 24" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M34 11H4.17391L13.1953 1.58374L11.4783 0L0 12L11.4783 24L13.0435 22.4348L4.17391 13H34V11Z"/>
+                      </svg>
+                    </button>
+                    <button class="main-section1-info-information-btn">
+                      <svg width="34" height="24" viewBox="0 0 34 24" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M0 11H29.8261L20.8047 1.58374L22.5217 0L34 12L22.5217 24L20.9565 22.4348L29.8261 13H0V11Z"/>
+                      </svg>
+                    </button>
+                  </div>
+                  <span class="main-section1-info-information-span">New Energy</span>
+                </div>
+              </figure>
+              <figure class="main-section1-info-cards"></figure>
+            </div>
+          </figure>
+          <figure class="main-section1-products">
+            <figure class="main-section1-products-card">
+              <span class="main-section1-products-card-num">01</span>
+              <div class="main-section1-products-card-wrap">
+                <span class="main-section1-products-card-title">Бесплатный звонок</span>
+                <p class="main-section1-products-card-description">На любые номера РФ</p>
+                <span class="main-section1-products-card-phone">8 800 550-19-80</span>
+                <button class="main-section1-products-card-btn">
+                  <svg width="30" height="31" viewBox="0 0 30 31" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M30 15.1304C30 23.0491 23.407 29.4684 15.2727 29.4684C12.6903 29.4684 10.2643 28.8208 8.15367 27.6843L0 30.2264L2.65833 22.5337C1.31733 20.3733 0.545 17.8393 0.545 15.1304C0.545 7.2117 7.13867 0.79248 15.273 0.79248C23.4077 0.79248 30 7.2117 30 15.1304ZM15.2727 3.0759C8.44467 3.0759 2.89067 8.48358 2.89067 15.1304C2.89067 17.7684 3.767 20.211 5.24933 22.1982L3.70233 26.6747L8.46067 25.1909C10.4157 26.4504 12.7577 27.1849 15.273 27.1849C22.1 27.1849 27.655 21.7779 27.655 15.1307C27.6547 8.4839 22.1003 3.0759 15.2727 3.0759ZM22.7097 18.4326C22.6187 18.2864 22.3783 18.1981 22.0177 18.0225C21.6563 17.8468 19.881 16.9962 19.551 16.8798C19.22 16.7624 18.9787 16.7035 18.7383 17.0551C18.498 17.407 17.806 18.1981 17.595 18.4326C17.3843 18.6677 17.174 18.6972 16.8127 18.5212C16.452 18.3456 15.289 17.9741 13.91 16.7768C12.837 15.8457 12.1123 14.6954 11.9017 14.3435C11.6913 13.992 11.8797 13.802 12.06 13.627C12.2227 13.4694 12.4213 13.2166 12.6017 13.0115C12.7827 12.8061 12.8427 12.6599 12.9623 12.4251C13.0833 12.1906 13.023 11.9856 12.9323 11.8093C12.8423 11.6337 12.1197 9.90425 11.819 9.20078C11.5183 8.49731 11.218 8.61439 11.007 8.61439C10.7967 8.61439 10.5557 8.58496 10.315 8.58496C10.0743 8.58496 9.683 8.67293 9.352 9.02451C9.02133 9.3764 8.089 10.2267 8.089 11.9558C8.089 13.6852 9.382 15.3561 9.563 15.5906C9.74333 15.8247 12.0597 19.4889 15.7297 20.8962C19.4 22.3025 19.4 21.8332 20.062 21.7743C20.7233 21.7158 22.1973 20.9243 22.4993 20.1041C22.7993 19.2826 22.7993 18.5791 22.7097 18.4326Z"/>
                   </svg>
+                  <span>+7 (928) 107-80-80</span>
                 </button>
               </div>
             </figure>
-            <figure class="main-info-cards"></figure>
-          </div>
-        </figure>
-        <figure class="main-products">
 
-        </figure>
+            <figure class="main-section1-products-card">
+              <span class="main-section1-products-card-num">02</span>
+              <div class="main-section1-products-card-wrap">
+                <span class="main-section1-products-card-title">Бесплатный подбор</span>
+                <p class="main-section1-products-card-description">Мы найдем самую низкую цену на оригинальную запчасть, достойную альтернативу или самый дешевый аналог.</p>
+                <button class="main-section1-products-card-btn">Заявка на подбор</button>
+              </div>
+            </figure>
+
+            <figure class="main-section1-products-card">
+              <span class="main-section1-products-card-num">03</span>
+              <div class="main-section1-products-card-wrap">
+                <span class="main-section1-products-card-title">Бесплатная доставка</span>
+                <p class="main-section1-products-card-description">Согласуем и привезём запчасти в офис или домой в удобное для Вас время.</p>
+                <button class="main-section1-products-card-btn">Пункты выдачи</button>
+              </div>
+            </figure>
+          </figure>
+        </section>
       </main>
     </div>
 
@@ -116,6 +167,31 @@ import { RouterLink, RouterView } from 'vue-router'
 
 
 <style lang="scss" scoped>
+@font-face {
+    font-family: gothampro700;
+    src: url(./assets/fonts/gothampro_bold.ttf);
+    font-weight: 700;
+    font-style: normal;
+}
+@font-face {
+    font-family: gothampro400;
+    src: url(./assets/fonts/gothampro_medium.ttf);
+    font-weight: 400;
+    font-style: normal;
+}
+@font-face {
+    font-family: gothampro300;
+    src: url(./assets/fonts/gothampro_light.ttf);
+    font-weight: 300;
+    font-style: normal;
+}
+@font-face {
+    font-family: gothampro900;
+    src: url(./assets/fonts/gothampro_black.ttf);
+    font-weight: 900;
+    font-style: normal;
+}
+
 
 /* Переменные (через $) */
 $colortext: #ffffff;
@@ -256,10 +332,144 @@ $colortext: #ffffff;
   width: 100%;
   padding: 40px 50px 0 50px;
   background-color: #f0f0f0;
-  background-image: url();
-  &-info{
+  &-section1{
     padding: 0 190px;
-    background: url(./assets/images/bgimg.png);
+    background: url(./assets/images/bgimg.png) no-repeat;
+    &-info{
+      &-information{
+        display: flex;
+        justify-content: space-between;
+        &-wrap--txt{
+          display: flex;
+          flex-direction: column;
+          &-btn{
+            text-align: right;
+          }
+        }
+        &-minilogo{ //мини лого
+        margin: 40px 0 35px 0;
+        }
+        &-surprise{ //фильтр в подарок
+          font-family: gothampro700;
+          font-size: 42px;
+          line-height: 49px;
+          color: #ffffff;
+        }
+        &-details{ // button Подробнее
+          width: 163px;
+          display: flex;
+          margin: 30px 0 40px 0;
+          padding: 0;
+          color: #222222;
+          border: none;
+          text-align: left;
+          background: none;
+
+          & span{
+            margin: 0 16px 0 0;
+            padding: 0;
+            font-family: gothampro400;
+            text-transform: uppercase;  
+            font-size: 16px;
+            line-height: 24px;
+          }
+          & svg{
+            fill: #1c69d4;
+          }
+        }
+        &-btn{ // right, left
+          width: 50px;
+          height: 50px;
+        }
+        &-span{ //New Energy
+          margin: 67px 0 0 0;
+          text-align: right;
+          font-family: gothampro300;
+          font-size: 75px;
+          line-height: 71px;
+          color: #ffffff;
+        }
+        &-oilbg{
+        position: absolute;
+        z-index: 0;
+        left: 804px;
+        top: 215px;
+        }
+      }
+    }
+    &-products{
+      display: flex;
+      padding: 0 0 28px 0;
+      position: relative;
+      &-card{
+        max-width: 460px;
+        max-height: 216px;
+        display: flex;
+        padding: 32px 20px 32px 16px;
+        background-color: #fff;
+
+        &:nth-child(2){
+          margin: 0 30px;
+        }
+        &-wrap{
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+        }
+        &-num{
+          font-family: gothampro900;
+          font-size: 36px;
+          line-height: 36px;
+          text-align: center;
+          margin: 0 8px 0 0;
+        }
+        &-title{
+          font-family: gothampro700;
+          font-size: 24px;
+          line-height: 24px;
+          text-align: left;
+        }
+        &-description{
+          font-family: gothampro400;
+          font-size: 14px;
+          line-height: 20px;
+          text-align: left;
+          padding: 8px 0;
+        }
+        &-phone{
+          font-family: gothampro400;
+          font-size: 24px;
+          line-height: 40px;
+          text-align: left;
+        }
+        &-btn{
+          padding: 13px 103px;
+          font-family: gothampro700;
+          font-size: 16px;
+          line-height: 23px;
+          text-align: center;
+          border-radius: 4px;
+          border: 2px solid #1c69d4;
+          color: #1c69d4;
+          background: none;
+          &:nth-child(4){
+            display: flex;
+            flex-direction: row;
+            padding: 13px 82px;
+            border: 2px solid #3ca255;
+            color: #3ca255;
+            & svg{
+              fill: #3ca255;
+              margin: 0 8px 0 0;
+            }
+            & span{
+              white-space: nowrap;
+              padding: 5px 0 0 0;
+            }
+          }
+        }
+      }
+    }
   }
 }
 
